6107d9f23c6f5efe57f32f25f3851503aeb4178d
Leandro-Tonello <leandronicolas1177@gmail.com>
2020-03-09 16:25:31 -0300

    Boostrap Agregado & Presentacion de Home

--- celuzone/views.py
+++ celuzone/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def post_list(request):
-    return render(request, 'celuzone/post_list.html', {})
+def home(request):
+    return render(request, 'celuzone/home.html', {})
--- mysite/settings.py
+++ mysite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'celuzone.apps.CeluzoneConfig',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
